e101961aec34430685429aa8083946a9a9954cc9
Chris Andrews <christiffer@gmail.com>
2023-12-07 23:15:37 +0000

    fix: Day 1 tests

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -79,8 +79,7 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let input = "";
-        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
         assert_eq!(result, Some(281));
     }
 }
